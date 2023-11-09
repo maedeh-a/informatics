810aeec77727bcc0caf7d17e91114bb1a9f620f6
Maedeh Asadi <maedeh.a7879@gmail.com>
2023-11-09 21:27:28 +0330

    dis

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 // Tab functionallity
 $(".tab-step .btn-step").click(function (e) {
+    const image_name = $(e.currentTarget).data('image-name');
+    if(!image_name) return;
     var test = $(e.currentTarget).data("tab");
     $(".tab-step .btn-step").removeClass("active");
     $(e.currentTarget).addClass("active");
     $(".tabs-items-step > div").addClass("d-none");
     $(".tabs-items-step  #" + test).removeClass("d-none");
-})
 
-$(".btn-step").on('click', function (e) {
-    console.log(e.currentTarget)
-    const image_name = $(e.currentTarget).data('image-name');
     const $img = $('.img-bg img')
     const src = $img.attr('src')
     const new_src = String(src).split('/').slice(0, -1).join('/') + "/" + image_name;
     $img.attr('src', new_src)
+
 })
